621fda04bb397fc5533caaf7dc5247a3c9d7f3db
Javier <897154@gmail.es>
2026-02-26 16:46:31 +0100

    Añadida la cabecera, el pop up del perfil y el pasar a board

--- Frontend web/src/Home.jsx
+++ Frontend web/src/Home.jsx
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import './Home.css'
 
 function Home({onStart}) {
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
 
-  return (
-    <div className='home-screen'>
-        <div className='top-menu'>
-            <svg width={100} height={100} viewBox='-50 -50 100 100'>
-                <circle class='profile' cx={0} cy={-5} r={40}/>
-                <circle class='level' cx={35} cy={-30} r={15}/>
-            </svg>
-            <h1 className='title'>RUMMIPLUS</h1>
-            <svg width={100} height={100} viewBox='-50 -50 100 100'>
-                <rect class='shop' x={0} y={-30} width={200} height={50} rx={6}/>
-            </svg>
+    const toggleProfile = () => setIsProfileOpen(!isProfileOpen);
+
+    return (
+        <div className='home-screen'>
+            <div className='top-menu'>
+                <svg width={100} height={100} viewBox='-50 -50 100 100'>
+                    <circle class='profile' cx={0} cy={-5} r={40} onClick={toggleProfile}/>
+                    <circle class='level' cx={35} cy={-30} r={15}/>
+                </svg>
+                <h1 className='title'>RUMMIPLUS</h1>
+                <svg width={150} height={100} viewBox='0 0 200 50'>
+                    <rect class='shop' x={0} y={-13} width={200} height={50} rx={20}/>
+                </svg>
+            </div>
+            {isProfileOpen && (
+                <div className="profile-overlay">
+                    <div className="profile-content">
+                        <h2>Nombre de usuario</h2>
+                    </div>
+                    <button class='close-button' onClick={toggleProfile}>X</button>
+                </div>
+            )}
+            <button onClick={onStart}>Jugar</button>
         </div>
-        <button onClick={onStart}>Jugar</button>
-    </div>
-  )
+    )
 }
 
 export default Home;
